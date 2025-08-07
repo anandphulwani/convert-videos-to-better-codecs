26ea12b8efb125d5ea953cf527bf23013dece51e
Anand Phulwani <anand_phulwani@yahoo.com>
2025-08-07 15:21:07 +0530

    Corrected BUG of removing `TMP_INPUT` instead of `TMP_PROCESSING`.

--- includes/cleanup_working_folders.py
+++ includes/cleanup_working_folders.py
@@ -4,11 +4,11 @@ from helpers.logging_utils import log
 from helpers.get_all_files_sorted import get_all_files_sorted
 from helpers.find_all_dirs import find_all_dirs
 from helpers.remove_empty_dirs import remove_empty_dirs
-from config import TMP_OUTPUT_ROOT, TO_ASSIGN, IN_PROGRESS, DONE_DIR, FAILED_DIR, LOGS_DIR, TMP_INPUT
+from config import TMP_OUTPUT_ROOT, TO_ASSIGN, IN_PROGRESS, DONE_DIR, FAILED_DIR, LOGS_DIR, TMP_PROCESSING
 
 def cleanup_working_folders():
     log("Cleaning up WORKING folders...")
-    for src, rel in get_all_files_sorted(TMP_INPUT):
+    for src, rel in get_all_files_sorted(TMP_PROCESSING):
         try:
             os.remove(src)
             log(f"Removed processed file from TMP_INPUT: {rel}", level="debug")
@@ -18,7 +18,7 @@ def cleanup_working_folders():
     # Remove empty directories with progress bar
     all_main_dirs = [TO_ASSIGN, IN_PROGRESS, os.path.dirname(IN_PROGRESS), 
                      DONE_DIR, FAILED_DIR, LOGS_DIR, os.path.dirname(LOGS_DIR), 
-                     TMP_INPUT, os.path.dirname(TMP_OUTPUT_ROOT)]
+                     TMP_PROCESSING, os.path.dirname(TMP_OUTPUT_ROOT)]
     all_dirs = find_all_dirs(all_main_dirs)
     removed = remove_empty_dirs(all_dirs)
     print(f"Removed {removed} empty directories.")
