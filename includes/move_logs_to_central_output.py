2436e568971bf96cd79efac3fbc1169005c42563
Anand Phulwani <anand_phulwani@yahoo.com>
2025-09-06 01:32:10 +0530

    Corrected the LOG_FILE, ERROR_LOG_FILE variable to shared state.

--- includes/move_logs_to_central_output.py
+++ includes/move_logs_to_central_output.py
@@ -3,17 +3,17 @@ import logging
 
 from helpers.copy_and_move_with_progress import move_with_progress
 from config import LOGS_DIR
-import helpers.logging_utils as logging_utils
+from helpers.logging_utils import setup_logging, log_files_shared_state
 from helpers.remove_path import remove_path
 
 def move_logs_to_central_output(restart_logging = False):
     try:
-        current_log_file = logging_utils.LOG_FILE
-        current_error_file = logging_utils.ERROR_LOG_FILE
+        current_log_file = log_files_shared_state.LOG_FILE
+        current_error_file = log_files_shared_state.ERROR_LOG_FILE
         remote_log_file = os.path.join(LOGS_DIR, os.path.basename(current_log_file))
         logging.shutdown()
         if restart_logging:
-            logging_utils.setup_logging()
+            setup_logging()
 
         move_with_progress(current_log_file, remote_log_file)
 
